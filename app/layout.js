f6338c01c2c77080c4fced35250474f29042ad51
varahikondapalli20-ui <varahikondapalli20@gmail.com>
2026-05-18 19:15:44 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -13,3 +13,13 @@ export default function RootLayout({ children }) {
     </html>
   );
 }
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        <meta name="p:domain_verify" content="fc095defc9cf75c86968ed48b4b8f9ba" />
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
